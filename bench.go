ab4f2f63a6b6da3fd72e1f9f411e41d2ba526b98
Jeffrey Nelson <jeff.nelson@getclever.com>
2013-09-18 18:22:47 -0700

    Implemented performing http requests during playback

--- bench.go
+++ bench.go
@@ -4,61 +4,48 @@ import (
 	"bytes"
 	"encoding/base64"
 	"encoding/csv"
+	"flag"
 	"fmt"
 	"io"
 	"net"
 	"net/http"
 	"os"
 	"strconv"
+	"sync"
 	"time"
 )
 
-type APIEvent struct {
-	Verb       string
-	RequestURL string
-	User       string
-	Time       int
+type RequestEvent struct {
+	Verb string
+	Path string
+	User string
+	Time int
 }
 
-type APIReader struct {
+type RequestEventReader struct {
 	r *csv.Reader
 }
 
-func NewAPIReader(r io.Reader) *APIReader {
-	return &APIReader{r: csv.NewReader(r)}
+func newRequestEventReader(r io.Reader) *RequestEventReader {
+	reader := &RequestEventReader{r: csv.NewReader(r)}
+	reader.r.TrailingComma = true
+	return reader
 }
 
-func (r *APIReader) Read() (event APIEvent, err error) {
+func (r *RequestEventReader) Read() (event RequestEvent, err error) {
 	line, err := r.r.Read()
 	if err != nil {
-		return APIEvent{}, err
+		return RequestEvent{}, err
 	}
 
 	time, err := strconv.Atoi(line[0])
 	if err != nil {
-		return APIEvent{}, err
+		return RequestEvent{}, err
 	}
-	return APIEvent{line[1], line[2], line[3], time}, nil
+	return RequestEvent{line[1], line[2], line[3], time}, nil
 }
 
-func replayAPIEvent(event APIEvent) {
-	url := fmt.Sprintf("%s%s", "https://api-staging.ops.getclever.com", event.RequestURL)
-	fmt.Println("URL: ", url)
-	fmt.Println("VERB: ", event.Verb)
-	req, _ := http.NewRequest(event.Verb, url, nil)
-	req.Header.Set("Authorization", fmt.Sprintf("Basic %s", base64.StdEncoding.EncodeToString([]byte(event.User+":"))))
-
-	client := &http.Client{}
-	r, err := client.Do(req)
-	if err != nil {
-		fmt.Println("Error: ", err)
-	} else {
-		fmt.Println("Status code: ", r.StatusCode)
-		r.Body.Close()
-	}
-}
-
-func CreateDialFunc(startTime time.Time, endTimeResult *int64) func(network, addr string) (net.Conn, error) {
+func createDialFunc(startTime time.Time, endTimeResult *int64) func(network, addr string) (net.Conn, error) {
 	return func(network, addr string) (net.Conn, error) {
 		conn, err := net.Dial(network, addr)
 		*endTimeResult = time.Now().Sub(startTime).Nanoseconds()
@@ -66,7 +53,7 @@ func CreateDialFunc(startTime time.Time, endTimeResult *int64) func(network, add
 	}
 }
 
-func TimeRequest(verb, url string) {
+func timeRequest(verb, url string) {
 	req, _ := http.NewRequest(verb, url, nil)
 
 	var connectEndTime, headersEndTime, contentEndTime int64
@@ -74,7 +61,7 @@ func TimeRequest(verb, url string) {
 
 	client := &http.Client{
 		Transport: &http.Transport{
-			Dial: CreateDialFunc(startTime, &connectEndTime),
+			Dial: createDialFunc(startTime, &connectEndTime),
 		},
 	}
 
@@ -96,11 +83,30 @@ func TimeRequest(verb, url string) {
 	fmt.Println("Content receive time:", (contentEndTime-headersEndTime)/1000000, "ms")
 }
 
-func main() {
-	fmt.Println("Starting log playback...")
+func replayRequestEvent(rootURL string, event RequestEvent) {
+	url := fmt.Sprintf("%s%s", rootURL, event.Path)
+	fmt.Println(event.Verb, url)
+	req, _ := http.NewRequest(event.Verb, url, nil)
+	if event.User != "" {
+		req.Header.Set("Authorization", fmt.Sprintf("Basic %s", base64.StdEncoding.EncodeToString([]byte(event.User+":"))))
+	}
 
+	client := &http.Client{}
+	r, err := client.Do(req)
+	if err != nil {
+		fmt.Println("Error: ", err)
+	} else {
+		fmt.Println("Status code: ", r.StatusCode)
+		r.Body.Close()
+	}
+}
+
+func parseAndReplay(r io.Reader, rootURL string, speed float64) {
 	var startTime time.Time
-	in := NewAPIReader(os.Stdin)
+	in := newRequestEventReader(r)
+
+	var mutex sync.Mutex
+	count := 0
 	for {
 		rec, err := in.Read()
 		if err == io.EOF {
@@ -113,9 +119,29 @@ func main() {
 			startTime = time.Now()
 		}
 
-		for int(time.Now().Sub(startTime).Nanoseconds()/1000000) < rec.Time {
-			time.Sleep(100000000)
+		for int(float64(time.Now().Sub(startTime)/time.Millisecond)*speed) < rec.Time {
+			time.Sleep(time.Duration(100) * time.Millisecond)
 		}
-		fmt.Println(rec.Time, rec.Verb, rec.RequestURL)
+		mutex.Lock()
+		count++
+		mutex.Unlock()
+		go func() { replayRequestEvent(rootURL, rec); mutex.Lock(); count--; mutex.Unlock() }()
 	}
+
+	for count > 0 {
+		time.Sleep(time.Duration(100) * time.Millisecond)
+	}
+}
+
+func main() {
+	speed := flag.Float64("speed", 1, "Sets multiplier for playback speed.")
+	flag.Parse()
+
+	if flag.Arg(0) == "" {
+		fmt.Println("Must specify a base URL.")
+		os.Exit(1)
+	}
+	fmt.Println("Starting playback...")
+
+	parseAndReplay(os.Stdin, flag.Arg(0), *speed)
 }
