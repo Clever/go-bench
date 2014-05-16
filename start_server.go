8eb41791aa903d5f671a486f28e6a274d9dbb72a
Jonathan Eldridge <jonathan.eldridge@clever.com>
2014-05-15 17:38:35 -0700

    Adds start_server helper for starting slowhttp server

---
+++ start_server.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"github.com/Clever/go-bench/slowhttp"
+	"fmt"
+)
+
+func main() {
+	server, err := slowhttp.StartServer()
+	if (err != nil) {
+		fmt.Println(err)
+	}
+	
+	// continue running until user input given
+	var input string
+    _, err2 := fmt.Scanf("%s", &input)
+	if (err != nil) {
+		fmt.Println(err2)
+	}
+	fmt.Println("Exiting.")
+	server.Close()
+}
