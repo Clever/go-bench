5d7aa3e91b4a34e52eacdfb580f5bfb3968ad73a
Jonathan Eldridge <jonathan.eldridge@clever.com>
2014-05-16 13:14:59 -0700

    Adds bench tests for eventToRequest method

--- bench_test.go
+++ bench_test.go
@@ -3,7 +3,9 @@ package main
 import (
 	slowhttp "github.com/Clever/go-bench/slowhttp"
 	"os"
+	"github.com/stretchr/testify/assert"
 	"testing"
+	"strings"
 )
 
 func assertNoError(err error, t *testing.T) {
@@ -11,10 +13,6 @@ func assertNoError(err error, t *testing.T) {
 		t.Error(err)
 		t.FailNow()
 	}
-}
-
-func TestRequest_1(t *testing.T) {
-
 }
 
 func TestPlayback_1(t *testing.T) {
@@ -26,4 +24,31 @@ func TestPlayback_1(t *testing.T) {
 
 	parseAndReplay(p, "http://127.0.0.1:8653", 1)
 	l.Close()
+}
+
+const rootURL = "http://127.0.0.1:8653"
+const verb = "GET"
+const path = "/test/path"
+const delay = 0
+const extra = ""
+func TestEventToRequestWithEmptyAuthHeader(t *testing.T) {
+	assertRequestMatchesExpected(t, "", nil)
+}
+
+func TestEventToRequestWithBasicAuthHeader(t *testing.T) {
+	assertRequestMatchesExpected(t, "Basic XXXX", []string{"Basic XXXX"})
+}
+
+func TestEventToRequestWithBearerAuthHeader(t *testing.T) {
+	assertRequestMatchesExpected(t, "Bearer YYYY", []string{"Bearer YYYY"})
+}
+
+func assertRequestMatchesExpected(t *testing.T, benchAuthParam string, expectedAuthHeader []string) {
+	requestEvent := RequestEvent{verb, path, benchAuthParam, delay, extra}
+	httpRequest := eventToRequest(rootURL, requestEvent)
+	assert.Equal(t, verb, httpRequest.Method, "Wrong HTTP method")
+	host := strings.Replace(rootURL, "http://", "", -1)
+	assert.Equal(t, host, httpRequest.Host, "Wrong root URL")
+	assert.Equal(t, path, httpRequest.URL.Path, "Wrong request path")
+	assert.Equal(t, expectedAuthHeader, httpRequest.Header["Authorization"], "Wrong authentication header")
 }
